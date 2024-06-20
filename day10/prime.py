97b2bf1bd62150a86fd73ec2e0102b4ef5f14f63
Deshmukh Advait Jaywant <116196933+Deshmukh-Advait-Jaywant@users.noreply.github.com>
2024-06-20 22:10:12 +0530

    prime.py

---
+++ day10/prime.py
@@ -0,0 +1,27 @@
+'''
+ip:
+    [4,8,14,22,36,68]
+
+    array in increasing order
+    all elements in array are non prime numbers
+
+    find largest prime number between two points
+    and sum of all those numbers should be the output
+
+'''
+def findPrime(m):
+    for i in range(2,(m//2)+1):
+        if m%i==0:
+            return 0
+    return m
+    
+
+l=[4,8,14,22,36,68]
+m=0
+res=[]
+for i in range(len(l)-1):
+    for j in range(l[i],l[i+1]):
+        m=max(findPrime(j),m)
+    res.append(m)
+print(res)    
+print(sum(res))
